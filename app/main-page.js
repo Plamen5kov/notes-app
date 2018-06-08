80c5af3940a61728fdc894e74c916cfd1e139450
Plamen5kov <plamen.krasimirov.petkov@gmail.com>
2018-06-08 20:25:51 +0300

    wip: changing view:

--- app/main-page.js
+++ app/main-page.js
@@ -1,41 +1,41 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
+const ObservableArray = require("tns-core-modules/data/observable-array")
+  .ObservableArray;
+const Observable = require("tns-core-modules/data/observable").Observable;
 
-/*
-NativeScript adheres to the CommonJS specification for dealing with
-JavaScript modules. The CommonJS require() function is how you import
-JavaScript modules defined in other files.
-*/ 
-var createViewModel = require("./main-view-model").createViewModel;
-
+const colors = ["red", "green", "blue"];
+debugger;
+const secondArray = new ObservableArray([
+  {
+    title: "The Da Vinci Code"
+  },
+  {
+    title: "Harry Potter and the Chamber of Secrets"
+  },
+  {
+    title: "The Alchemist"
+  },
+  {
+    title: "The Godfather"
+  },
+  {
+    title: "Goodnight Moon"
+  },
+  {
+    title: "The Hobbit"
+  }
+]);
 function onNavigatingTo(args) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-    var page = args.object;
+  debugger;
+  const page = args.object;
+  const vm = new Observable();
+  var notesList = page.findViewById("lv_notes");
+  console.log(notesList);
+  notesList.refresh();
 
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and JavaScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
+  vm.set("myItems", colors);
+  vm.set("mySecondItems", secondArray);
 
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
-    page.bindingContext = createViewModel();
+  page.bindingContext = vm;
 }
 
-/*
-Exporting a function in a NativeScript code-behind file makes it accessible
-to the file’s corresponding XML file. In this case, exporting the onNavigatingTo
-function here makes the navigatingTo="onNavigatingTo" binding in this page’s XML
-file work.
-*/
-exports.onNavigatingTo = onNavigatingTo;
+module.export = onNavigatingTo;
